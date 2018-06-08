2b0391a91fb1273dd17696ada7fb38618fa9fb14
elifer aponte <eliferaponte_11@hotmail.com>
2018-06-07 12:29:15 -1200

    memotest

---
+++ memotest/js/index.js
@@ -0,0 +1,37 @@
+const container = document.getElementsByClassName("user");
+function go(ruta) {
+    window.location.href = ruta;
+}//me dirije a la sig pág luego de completar el campo
+
+function saveName() {
+
+    const input   = document.getElementById('name');
+    const name    = input.value;
+    const noNameEl = $('#noName').length;
+
+    if(!name && !noNameEl) {
+        $(".user").append("<p id ='noName'>¡Hey, escribe tu nombre!</p>");
+        return;
+    } else if(!name) {
+        return;
+    }
+
+    localStorage.setItem('name', name);
+
+    go("./nivel.html");
+}
+
+function isUserExists() {
+
+    const userName = localStorage.getItem('name');
+
+    if (!userName) {
+        go("./index.html");
+        return;
+    }
+}
+
+function goBack() {
+    go('./index.html');
+    return;
+}
